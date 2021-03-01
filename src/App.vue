ab3917f97d2c43a2529f42e436f1d08159a7955e
amandhaka <amandhaka2021@gmail.com>
2021-03-01 12:33:00 +0530

    Core Vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,68 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>{{message}}</h1>
+    <div v-html="btnhtml"></div>
+    <input type="text" v-model="text">
+    <p v-if="text==='Aman'">Hey Aman</p>
+    <p v-else>Wrong person</p>
+    <h3>{{text}}</h3>
+    <button v-on:click="update">Update</button>
+    <h2>{{count}}</h2>
+    <h3>{{topicNum}}</h3>
+    <br>
+    <input type="text" placeholder="Task" v-model="task">
+    <button @click="updateTodo()">Add</button>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{todo}}
+      </li>
+    </ul>
+    <input type="text" placeholder="Title" v-model="title">
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      count: 0,
+      message: 'Clicked',
+      btnhtml: '<button>Click</button>',
+      text: '',
+      arr: ['java', 'c++', 'python'],
+      todos: [],
+      task: '',
+      title: ''
     }
+  },
+  methods: {
+    update () {
+      return this.count++
+    },
+    updateTodo () {
+      this.todos.push(this.task)
+      this.task = ''
+    }
+  },
+  watch: {
+    title (newVal, oldVal) {
+      alert(newVal + ' ' + oldVal)
+    }
+  },
+  computed: {
+    topicNum () {
+      return this.arr.length
+    }
+  },
+  created () {
+    console.log('Created')
+  },
+  mounted () {
+    setInterval(() => { this.count++ }, 100)
   }
 }
+</script>
+
+<style lang="scss">
 </style>
